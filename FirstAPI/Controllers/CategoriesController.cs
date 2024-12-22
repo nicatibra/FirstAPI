1cd91261402ad58080bc6a0e023ccaabb4d159d6
nicatibra <nicatibrahimlibusiness@gmail.com>
2024-12-22 21:59:47 +0400

    Inital

---
+++ FirstAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,96 @@
+﻿using FirstAPI.Data;
+using FirstAPI.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categories = await _context.Categories.ToListAsync();
+
+            return StatusCode(StatusCodes.Status200OK, categories);
+            //return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return StatusCode(StatusCodes.Status200OK, category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status201Created, category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, string name)
+        {
+            if (id < 1)
+            {
+                return BadRequest();
+            }
+
+            Category category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            category.Name = name;
+
+            await _context.SaveChangesAsync();
+
+            return StatusCode(StatusCodes.Status204NoContent);
+        }
+    }
+}
---
+++ FirstAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using FirstAPI.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
